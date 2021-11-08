e578c2dc697dca91f04a86ce0515a7b951754f05
Reetta Ylitalo <reetta.k.ylitalo@helsinki.fi>
2021-11-08 17:48:00 +0200

    Laskarit viikko 2: maksukortin testit

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,45 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertTrue(kortti.saldo() == 10);
+    }
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(10);
+        
+        assertTrue(kortti.saldo() == 20);
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinJosRahaaTarpeeksi() {
+        kortti.otaRahaa(5);
+        
+        assertTrue(kortti.saldo() == 5);
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRahaaEiTarpeeksi() {
+        kortti.otaRahaa(20);
+        
+        assertTrue(kortti.saldo() == 10);
+    }
+    
+    @Test
+    public void trueJosRahaRiittaa() {
+        assertTrue(kortti.otaRahaa(8));
+    }
+    
+    @Test
+    public void falseJosRahaEiRiita() {
+        assertFalse(kortti.otaRahaa(80));
+    }
+    
+    @Test
+    public void kortinTulostusToimii() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
 }
